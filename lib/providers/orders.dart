70c0df916264328efd5d67755f868e5eadae8501
Ubaid Badar <m.ubaidbadar@gmail.com>
2020-06-17 13:24:52 +0500

    store_orders_in_db_and_start_fetch_orders_from_db

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
-import 'package:myshop/providers/cart.dart';
+import '../providers/cart.dart';
+import '../constants/db_url.dart';
+import 'package:http/http.dart' as http;
+
+const _dburl = "$db_url/orders";
 
 class Order {
   final String id;
@@ -17,16 +22,54 @@ class Order {
 class Orders with ChangeNotifier {
   final List<Order> _orders = [];
   List<Order> get orders => [..._orders];
-  void addOrder(List<CartItem> cart, double price) {
-    _orders.insert(
-      0,
-      Order(
-        cart: cart,
-        price: price,
-        id: DateTime.now().toString(),
-        dateTime: DateTime.now(),
-      ),
+
+  String _toJson(Order order) {
+    return json.encode({
+      'price': order.price,
+      'dateTime': order.dateTime.toIso8601String(),
+      'products': order.cart
+          .map((ci) => json.encode({
+                'title': ci.title,
+                'price': ci.price,
+                'quantity': ci.quantity,
+                'id': ci.id,
+              }))
+          .toList(),
+    });
+  }
+
+  Future<void> addOrder(List<CartItem> cart, double price) {
+    final order = Order(
+      cart: cart,
+      price: price,
+      id: DateTime.now().toString(),
+      dateTime: DateTime.now(),
     );
-    notifyListeners();
+
+    return http.post('$_dburl.json', body: _toJson(order)).then((res) {
+      _orders.insert(
+        0,
+        Order(
+          id: json.decode(res.body)['name'],
+          cart: cart,
+          price: price,
+          dateTime: order.dateTime,
+        ),
+      );
+      notifyListeners();
+    });
+  }
+
+  Future<void> refreshOrders(){
+    return http.get('$_dburl.json').then((res){
+      print(json.decode(res.body));
+    });
+  }
+
+  Future<void> fetchAndSetOrders(){
+    if(_orders.length > 0){
+      return refreshOrders();
+    }
+    return Future.delayed(Duration.zero);
   }
 }
